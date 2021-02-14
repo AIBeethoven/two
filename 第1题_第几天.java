fb15e146d5a2ddef0125e5acc35c64aa8fd311a1
AIBeethoven <50749666+AIBeethoven@users.noreply.github.com>
2021-02-14 19:13:18 +0800

    Create 第1题_第几天.java

---
+++ 第1题_第几天.java
@@ -0,0 +1,14 @@
+package 第九届.省赛.javaB;
+
+public class 第1题_第几天 {
+	public static void main(String[] args) {
+		/*
+		2000年的1月1日，是那一年的第1天。
+		那么，2000年的5月4日，是那一年的第几天？
+		*/
+		int answer = 31 + 29 + 31 + 30 + 4;
+		System.out.println(answer);
+		
+	}
+	
+}
